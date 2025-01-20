a2eee41be4988181b6f26a497131f7127539a13b
Aravind Edakkot <73080540+AravindEdakkot@users.noreply.github.com>
2025-01-21 01:26:41 +0530

    Update App.js
    aravind REACt project

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aravind REACT PROJECT
         </a>
       </header>
     </div>
